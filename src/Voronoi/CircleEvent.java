5e72e9ee731ddbdedf7282aa791c1abc04b4694b
tezm <mnmichtest6@gmial.com>
2018-05-06 20:25:07 +0200

    Scheme of classes for two types of events;

---
+++ src/Voronoi/CircleEvent.java
@@ -0,0 +1,18 @@
+package Voronoi;
+
+/*
+    Circle event
+*/
+
+public class CircleEvent extends Event{
+
+    CircleEvent(Point point) {
+        super(point);
+    }
+
+    @Override
+    public void handle() {
+        /* to implement */
+    }
+
+}
---
+++ src/Voronoi/SiteEvent.java
@@ -0,0 +1,17 @@
+package Voronoi;
+
+/*
+    Site event
+*/
+
+public class SiteEvent extends Event {
+
+    SiteEvent(Point point) {
+        super(point);
+    }
+
+    @Override
+    public void handle() {
+        /* to implement */
+    }
+}
