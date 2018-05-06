cc999b348a86abfbb835da6f3104634de85eb2d0
tezm <mnmichtest6@gmial.com>
2018-05-06 20:17:25 +0200

    Abstract class for events in Fortune's algorithm.

--- src/Voronoi/Event.java
+++ src/Voronoi/Event.java
@@ -1,4 +1,31 @@
 package Voronoi;
 
-public class Event {
+/*
+    Abstract class Event for implementing sweeping queue and events in Forunne's Algorithm.
+*/
+
+public abstract class Event implements Comparable <Event> {
+    /*
+        point: point of Event
+    */
+
+    public final Point point;
+
+    Event(Point point) {
+        this.point =  point;
+    }
+
+    public abstract void handle(/* to fill */);
+
+    @Override
+    public int compareTo(Event other) {
+        return Integer.compare(other.point.getY(), point.getY());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || getClass() != other.getClass()) return false;
+        if (other == this) return true;
+        return point.equals(((Event) other).point);
+    }
 }
